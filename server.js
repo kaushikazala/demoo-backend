70d55dd79188c023e1f2d0a022e208535864d2d1
kaushikazala <kaushikazala4@gmail.com>
2026-06-17 14:05:53 +0530

    cores

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://127.0.0.1:5173',
-  process.env.FRONTEND_URL_LOCAL,
-  process.env.FRONTEND_URL_PROD,
+  'https://demoo-frontend-phi.vercel.app/'
+  // process.env.FRONTEND_URL_LOCAL,
+  // process.env.FRONTEND_URL_PROD,
 ].filter(Boolean);
 
 const corsOptions = {
